9471a592c6ff3be0d86161dcaf3387a9b2fae174
Felix Ouma <fatonoh@gmail.com>
2022-05-23 18:56:09 +0300

    Add remove book function

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,32 +3,56 @@ const author = document.getElementById('author');
 const dataContainer = document.getElementById('list-books');
 const form = document.forms[0];
 
-const bookObj = [];
+let bookObj = [];
 
-function copyvalues(e) {
-    e.preventDefault();
-    bookObj.push({
-        title: title.value,
-        author: author.value
-    });
-    displayBooks(bookObj);
-    form.reset();
-    return;
+function populateLocalStorage() {
+  const bookStore = JSON.stringify(bookObj);
+  localStorage.setItem('bookstore', bookStore);
+}
+
+function deleteBook(event) {
+  const bookId = event.target.id;
+  bookObj.splice(bookId, 1);
+  populateLocalStorage();
+  displayBooks(bookObj);
 }
 
 function displayBooks(books) {
-    let newObj = books[books.length - 1];
-    let listofbooks = dataContainer.innerHTML;
-    listofbooks += `<article class="book">
-  <div>${newObj.title}</div>
-  <div>${newObj.author}</div>
-  <div>
-      <button type="submit" id="rm-btn">Remove</button>
-  </div>
-  <hr>
-</article>
-  `
-    dataContainer.innerHTML = listofbooks;
+  let listOfBooks = '';
+
+  books.forEach((book, index) => {
+    listOfBooks += `<article class="book">
+     <div>${book.title}</div>
+     <div>${book.author}</div>
+     <div>
+         <button type="submit" class="btn-delete" id=${index}>Remove</button>
+     </div>
+     <hr>
+   </article>`;
+  });
+  dataContainer.innerHTML = listOfBooks;
+  if (bookObj.length > 0) {
+    const deleteButtons = document.querySelectorAll('.btn-delete');
+    deleteButtons.forEach((button) => button.addEventListener('click', deleteBook));
+  }
 }
+
+function copyvalues(e) {
+  e.preventDefault();
+  bookObj.push({
+    title: title.value,
+    author: author.value,
+  });
+  populateLocalStorage();
+
+  displayBooks(bookObj);
+  form.reset();
+}
+
+window.onload = () => {
+  const data = JSON.parse(localStorage.getItem('bookstore'));
+  bookObj = data;
+  displayBooks(bookObj);
+};
 
 form.addEventListener('submit', copyvalues);

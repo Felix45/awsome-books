c4d765c819a69e78fef1fe25beabdc446041d352
Waleed <waleedamjad99@hotmail.com>
2022-05-24 18:31:00 +0500

    Refactor addBook and displayBook functions

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,60 +1,38 @@
-const title = document.getElementById('title');
-const author = document.getElementById('author');
-const dataContainer = document.getElementById('list-books');
+class BookStore {
+  constructor() {
+    this.objBooks = JSON.parse(localStorage.getItem('bookstore')) || [];
+  }
+
+  addBook(title, author) {
+    this.objBooks.push({ title, author });
+    this.populateLocalStorage();
+    this.displayBooks();
+  }
+
+  displayBooks() {
+    let listOfBooks = '';
+    this.objBooks.forEach((book, index) => {
+      listOfBooks += `
+      <article>
+        <div>${book.title}</div>
+        <div>${book.author}</div>
+        <div>
+          <button class="btn-delete" id=${index}>Remove</button>
+        </div>
+        <hr/>
+      </article>`;
+    });
+    document.querySelector('.list-of-books').innerHTML = listOfBooks;
+    this.registerDeletebuttons();
+  }
+}
+const bookstore = new BookStore();
 const form = document.forms[0];
-
-let bookObj = [];
-
-function populateLocalStorage() {
-  const bookStore = JSON.stringify(bookObj);
-  localStorage.setItem('bookstore', bookStore);
-}
-
-function deleteBook(event) {
-  const bookId = event.target.id;
-  bookObj.splice(bookId, 1);
-  populateLocalStorage();
-  displayBooks(bookObj);
-}
-
-function displayBooks(books) {
-  let listOfBooks = '';
-
-  books.forEach((book, index) => {
-    listOfBooks += `<article class="book">
-     <div>${book.title}</div>
-     <div>${book.author}</div>
-     <div>
-         <button type="submit" class="btn-delete" id=${index}>Remove</button>
-     </div>
-     <hr>
-   </article>`;
-  });
-  dataContainer.innerHTML = listOfBooks;
-  if (bookObj.length > 0) {
-    const deleteButtons = document.querySelectorAll('.btn-delete');
-    deleteButtons.forEach((button) => button.addEventListener('click', deleteBook));
-  }
-}
-
-function copyvalues(e) {
-  e.preventDefault();
-  bookObj.push({
-    title: title.value,
-    author: author.value,
-  });
-  populateLocalStorage();
-
-  displayBooks(bookObj);
-  form.reset();
-}
-
 window.onload = () => {
-  const data = JSON.parse(localStorage.getItem('bookstore'));
-  if (data) {
-    bookObj = data;
-    displayBooks(bookObj);
-  }
+  bookstore.displayBooks();
 };
-
-form.addEventListener('submit', copyvalues);
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  bookstore.addBook(form.elements.title.value, form.elements.author.value);
+  form.reset();
+});
